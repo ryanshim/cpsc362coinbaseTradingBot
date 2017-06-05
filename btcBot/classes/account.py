ca5152e6e468b1e10b64489714d6ffd8bd56b710
ryanshim <ryan.shim@gmail.com>
2017-06-05 14:00:12 -0700

    added account class file

---
+++ btcBot/classes/account.py
@@ -0,0 +1,29 @@
+'''
+Account class: any member variables and functions related to a
+bitcoin account will go here
+'''
+import json
+import requests
+from coinbasewalletauth import CoinbaseWalletAuth
+
+class Account():
+    def __init__(self):
+        # initialize generic api url
+        self.api_url = "https://api.coinbase.com/v2/"
+
+        # initial setup
+        auth = CoinbaseWalletAuth() 
+
+    def get_acct_name(self):
+        # add acct name function here
+        return
+
+    def get_acct_id(self):
+        # add acct id function here
+        return
+
+    def get_acct_balance(self):
+        # get current balance in this form (balance, currency_type)
+        # the variables in the tuple above are string types
+        return
+
