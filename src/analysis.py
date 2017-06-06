22211e19573e806e30e85750f39036b36ba00f3f
Ryan <ryan.shim@gmial.com>
2017-06-06 11:01:07 -0700

    added analysis class

---
+++ src/analysis.py
@@ -0,0 +1,11 @@
+'''
+Analysis class:
+Performs analysis on historical btc prices to create
+a buy/sell sentiment. Might need to do linear regression
+and hypothesis testing for sentiment
+'''
+import numpy
+
+class Analysis:
+    def __init__(self):
+        # define mem var
