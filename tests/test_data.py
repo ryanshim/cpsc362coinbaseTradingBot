4f86ff4d52d84d5b5b9b0614bc7b644e9f1d3d07
Ryan <ryan.shim@gmial.com>
2017-06-08 10:39:11 -0700

    updated test suite for data class

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,9 +4,28 @@ Data class unit tests
 import sys
 sys.path.append('../src/')
 import unittest
-from account import Account
+from data import Data
 
 class DataTest(unittest.TestCase):
     def test_get_spot_price(self):
-        # test retrieving current market price
+        data_1 = Data()
+        self.assertEqual('data_1.get_spot_price()', 'data_1.get_spot_price()')
 
+    def test_get_price_hist_6_mon():
+        data_1 = Data()
+        self.assertEqual(data_1.get_price_hist_6_mon(), '')
+
+    def test_get_price_hist_cur_mon():
+        data_1 = Data()
+        self.assertEqual(data_1.get_price_hist_cur_mon(), '')
+
+    def test_get_price_hist_cur_wk():
+        data_1 = Data()
+        self.assertEqual(data_1.get_price_hist_cur_wk(), '')
+
+    def test_get_price_hist_cur_hr():
+        data_1 = Data()
+        self.assertEqual(data_1.get_price_hist_cur_hr(), '')
+
+if __name__ == '__main__':
+    unittest.main()
