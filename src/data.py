e3963eb7070f8bdbfdd0ea3fbf7ef7db9d3b5bac
ryanshim <ryan.shim@gmail.com>
2017-06-09 17:14:16 -0700

    added get_exchange_rates() function

--- src/data.py
+++ src/data.py
@@ -19,7 +19,7 @@ class Data():
         self.spot_price_currency = ''
 
     # current market btc price
-    def get_spot_price():
+    def get_spot_price(self):
         auth = CoinbaseWalletAuth()
         req = requests.get(self.api_url + 'prices/BTC-USD/spot', auth=auth)
         output = dict(req.json())
@@ -31,20 +31,38 @@ class Data():
 
     # get historical prices of beg each month
     # from the last 6 months
-    def get_price_hist_6_mon():
+    def get_price_hist_6_mon(self):
         return
 
     # get historical prices of beg of each week
     # from the current month
-    def get_price_hist_cur_mon():
+    def get_price_hist_cur_mon(self):
         return
 
     # get historical prices of beg of each day
     # from the current week
-    def get_price_hist_cur_wk():
+    def get_price_hist_cur_wk(self):
         return
 
     # get historical prices of each hour
     # from current day to the current hour
-    def get_price_hist_cur_hr():
+    def get_price_hist_cur_hr(self):
         return
+
+    # get the exchange ranges for the top 5 most used currencies
+    def get_exchange_rates(self, convert_amt):
+        auth = CoinbaseWalletAuth()
+        req = requests.get(self.api_url + 'exchange-rates', auth=auth).json()
+        output = dict(req['data']['rates'])
+
+        exchange_list = []
+        for k,v in output.iteritems():
+            if (key == 'BTC'):
+                exchange_list.append("%.6f" % (float(convert)*float(value)))
+                exchange_list.append(key)
+            if ((key == 'USD') or (key == 'EUR') or (key == 'JPY') or (key == 'GBP') or (key == 'CHF')):
+                exchange_list.append("%.2f" % (float(convert)*float(value)))
+                exchange_list.append(key)
+
+        return exchange_list
+
