d7e02e27bd5cc71a845e40236bc2184861d95a3a
ryanshim <ryan.shim@gmail.com>
2017-06-10 21:35:43 -0700

    updated data class unit tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -11,21 +11,25 @@ class DataTest(unittest.TestCase):
         data_1 = Data()
         self.assertEqual('data_1.get_spot_price()', 'data_1.get_spot_price()')
 
-    def test_get_price_hist_6_mon():
+    def test_get_price_hist_6_mon(self):
         data_1 = Data()
         self.assertEqual(data_1.get_price_hist_6_mon(), '')
 
-    def test_get_price_hist_cur_mon():
+    def test_get_price_hist_cur_mon(self):
         data_1 = Data()
         self.assertEqual(data_1.get_price_hist_cur_mon(), '')
 
-    def test_get_price_hist_cur_wk():
+    def test_get_price_hist_cur_wk(self):
         data_1 = Data()
         self.assertEqual(data_1.get_price_hist_cur_wk(), '')
 
-    def test_get_price_hist_cur_hr():
+    def test_get_price_hist_cur_hr(self):
         data_1 = Data()
         self.assertEqual(data_1.get_price_hist_cur_hr(), '')
+
+    def test_get_exchange_rates(self):
+        data_1 = Data()
+        self.assertEqual(data_1.get_exchange_rates(), '')
 
 if __name__ == '__main__':
     unittest.main()
